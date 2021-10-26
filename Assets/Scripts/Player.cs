1e3516a2d9a9d8b659facd99b57d0599975bd4de
MJA <mja@alzago.com>
2021-10-26 18:19:48 +0200

    Unidad UNI-200-02 completa

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,21 +6,53 @@ public class Player : MonoBehaviour
 {
     [SerializeField] float movHor;
     [SerializeField] float speed = 5f;
+    [SerializeField] bool isMoving = false;
+    [SerializeField] float jumpForce = 3f;
+    [SerializeField] bool isGrounded = false;
+    [SerializeField] float radius = 0.3f;
+    [SerializeField] float groundRayDist = 0.5f;
+    [SerializeField] LayerMask groundLayer;
 
     Rigidbody2D rb;
-    // Start is called before the first frame update
     void Start()
     {
         rb = this.GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         movHor = Input.GetAxisRaw("Horizontal");
+        isMoving = (movHor != 0f);
+        isGrounded = Physics2D.CircleCast(this.transform.position, radius, Vector3.down, groundRayDist, groundLayer);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            jump();
+        }
+        flip(movHor);
     }
     void FixedUpdate()
     {
         rb.velocity = new Vector2(movHor * speed, rb.velocity.y);    
+    }
+    void jump()
+    {
+        if (!isGrounded) return;
+
+        rb.velocity = Vector2.up * jumpForce;
+    }
+    void flip(float direccion)
+    {
+        
+    }
+
+    private void OnDrawGizmos()
+    {
+        RaycastHit2D rayo;
+        Gizmos.color = Color.yellow;
+        rayo = Physics2D.CircleCast(this.transform.position, radius, Vector3.down, groundRayDist, groundLayer);
+
+        Gizmos.DrawSphere(rayo.centroid, rayo.distance);
+        
     }
 }
